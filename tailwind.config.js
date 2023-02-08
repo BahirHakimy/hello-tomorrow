ffb855f046ca05a464d986010e80fad3565bfdc2
Bahir Hakimy <bahirhakimy2015@gmail.com>
2023-02-08 21:09:12 +0430

    Update index.html about.html desktop version completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,10 @@ module.exports = {
       colors: {
         primary: '#ec5242',
         'primary-transparent': '#ec524299',
-        secodary: '#272a31',
+        secondary: '#272a31',
+        walnut: '#3e3e3e',
         light: '#d3d3d3',
+        'light-transparent': '#d3d3d366',
         'dark-smoke': '#4c4c4c',
       },
       fontFamily: { Lato: 'Lato', Cocogoose: 'Cocogoose' },
